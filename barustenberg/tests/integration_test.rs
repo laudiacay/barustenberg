20d90b362d3dec6739031f15f63d168a11045df4
Claudia Richoux <c@laudiacay.cool>
2023-09-18 01:51:06 -0400

    various test cleanup

--- barustenberg/tests/integration_test.rs
+++ barustenberg/tests/integration_test.rs
@@ -1,11 +1,11 @@
 #[test]
 #[ignore]
 fn test_add() {
-    assert_eq!(barustenberg::add(3, 2), 5);
+    todo!();
 }
 
 #[test]
 #[ignore]
 fn test_mult() {
-    assert_eq!(barustenberg::mult(3, 2), 6);
+    todo!();
 }
